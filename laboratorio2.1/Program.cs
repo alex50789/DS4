c38eae486011e5c7de8b9114a0d3cce481b2db48
alex50789 <alexander.toribio@utp.ac.pa>
2024-08-31 21:49:26 -0500

    laboratorio 2.1

---
+++ laboratorio2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace laboratorio21
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        public static int Valor;
+    }
+    
+}
