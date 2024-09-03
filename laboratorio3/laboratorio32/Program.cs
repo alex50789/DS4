0e9f0335f2fa3783008ded5fee09d02d064f6020
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-02 20:33:36 -0500

    laboratorio32

---
+++ laboratorio3/laboratorio32/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int radio;
+
+        Console.WriteLine("Ingrese el radio del circulo: ");
+        radio = Convert.ToInt32(Console.ReadLine());
+
+        CalcularArea ca = new CalcularArea(radio);
+
+        Console.WriteLine("la respuesta de la operacion es: " + ca.Calcular());
+    }
+
+    public class CalcularArea
+    {
+        private int r;
+        private double a;
+
+        public CalcularArea(int R)
+        {
+            this.r = R;
+        }
+
+        public double Calcular()
+        {
+            double pi = 3.14;
+            a = pi + (r * r);
+            return a;
+        }
+    }
+}
