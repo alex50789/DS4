3cc20fe59fb761c72c3362c542ff3025b03b8a2c
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-02 20:55:11 -0500

    laboratorio33

---
+++ laboratorio3/laboratorio33/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int largo, ancho;
+
+        Console.WriteLine("Ingrese el largo del rectangulo: ");
+        largo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el ancho del rectangulo: ");
+        ancho = Convert.ToInt32(Console.ReadLine());
+
+        CalcularPerimetro cp = new CalcularPerimetro(largo, ancho);
+
+        Console.WriteLine("la respuesta de la operacion es: " + cp.Calcular());
+    }
+
+    public class CalcularPerimetro
+    {
+        private int l;
+        private int a;
+        private int r;
+
+        public CalcularPerimetro(int L, int A)
+        {
+            l = L;
+            a = A;
+        }
+
+        public int Calcular()
+        {
+            r = 2*(l+a);
+            return r;
+        }
+
+    }
+}
