61291a19b90731aeb17caf540466a2d4ed8c6b16
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-02 20:09:11 -0500

    laboratorio31

---
+++ laboratorio3/laboratorio3/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero;
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        CalculoMatematico op = new CalculoMatematico(primerNumero, segundoNumero);
+
+        Console.WriteLine("la respuesta de la operacion es: " + op.Calcular());
+    }
+
+    public class CalculoMatematico
+    {
+        private int n1;
+        private int n2;
+        private int resp;
+
+        public CalculoMatematico(int N1, int N2)
+        {
+            n1 = N1;
+            n2 = N2;
+        }
+
+        public int Calcular()
+        {
+            resp = (n1+n2)*(n1-n2) ;
+            return resp;
+        }
+    }
+}
