55f35edb0492c24086990d2eff60effee8b92e53
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-09 19:41:07 -0500

    parte 4.1

---
+++ laboratorio4/lab4/lab4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab4
+{
+    class program
+    {
+        private static void Main(string[] args)
+        {
+            int n, x;
+            string linea;
+            Console.Write("ingrese el valor de n:");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            x = 1;
+            while (x <= n) 
+            {
+                Console.Write(x);
+                Console.Write(" , ");
+                x = x + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
