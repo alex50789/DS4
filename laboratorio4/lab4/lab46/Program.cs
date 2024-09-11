b8c01e4ec20facb7d2fee1a3d067b3a62ed0310f
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-11 18:36:00 -0500

    parte 4.7

---
+++ laboratorio4/lab4/lab46/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace labs46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el radio del Circulo");
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = Math.Pow(radio, 2) * Math.PI;
+
+            Console.WriteLine($"el area del circulo es: {area}");
+        }
+    }
+}
