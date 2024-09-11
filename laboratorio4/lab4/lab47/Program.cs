3a23ff1821ed126607b3bcaef566cb0015dfc03d
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-11 18:38:45 -0500

    parte 4.7

---
+++ laboratorio4/lab4/lab47/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Labs47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 70;
+            double numero2 = 67.89, numero3 = 67.89;
+            Console.WriteLine(Suma(numero1, numero3));
+            Console.WriteLine(Suma(numero1, numero2, numero3));
+
+        }
+        static double Suma(int x, double y, double z = 0)
+        {
+            return x + y + z;
+
+        }
+    }
+}
