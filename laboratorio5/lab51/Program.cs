a917ba6a5ca1dc4ed3a39cb35abd8c577f5d8bc2
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-11 19:37:19 -0500

    parte 5.1

---
+++ laboratorio5/lab51/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+private int[] sueldos;
+
+public void Carga()
+{
+    sueldos = new int[6];
+    for (int f = 1; f <= 5; f++)
+    {
+        Console.Write("Ingrese sueldos del operario " + f + ": ");
+        String linea;
+        linea = Console.ReadLine();
+        sueldos[f] = int.Parse(linea);
+    }
+}
+
+public void Imprimir()
+{
+    Console.Write("Los 5 sueldos de los operarios \n");
+    for (int f = 1;f <= 5;f++)
+    {
+        Console.Write("[" + sueldos[f]+"]");
+    }
+    Console.ReadKey();
+}
+
+static void Main(string[] args)
+{
+    Program pv = new Program();
+    pv.Carga();
+    pv.Imprimir();
+}
+}
