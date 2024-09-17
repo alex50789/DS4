b980484f3a7a1233f78e09d3e74a7d041a79ad01
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-16 19:32:00 -0500

    parte 6.2

---
+++ laboratorio6/lab62/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("digite el numero deseado");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("no se ha introduvido un numero valido");
+            num = -1;
+
+        }
+        catch (OverflowException ex) { 
+            Console.WriteLine("El numero introducido es muy grande");
+            num = -1;
+
+        }
+
+        Console.WriteLine(num);
+    }
+}
