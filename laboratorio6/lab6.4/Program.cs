e6ec640c0de2f9a995633f795693eb9219ff9fb1
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-16 19:49:01 -0500

    parte 6.4

---
+++ laboratorio6/lab6.4/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    private static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso denegado - No cumple con el criterio de edad");
+
+        }
+        else
+        {
+            Console.WriteLine("Acceso concedido");
+        }
+    }
+
+    private static  void Main(string[] args)
+    {
+        checkAge(25);
+    }
+}
