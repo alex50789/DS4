594ce8cb7348353135777a4c4e415bc919fd234e
alex50789 <alexander.toribio@utp.ac.pa>
2024-09-16 19:19:07 -0500

    parte 6.1

---
+++ laboratorio6/lab6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        int num; Console.WriteLine("digite el numero deseado: ");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+
+        }
+        catch (FormatException ex) { 
+            Console.WriteLine("no ha introducido un digito valido");
+            num = -1;
+        }
+        Console.WriteLine(num);
+    }
+}
