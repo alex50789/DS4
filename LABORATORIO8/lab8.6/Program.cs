dd7992031f5b080b43426f32b31e733d73863cc3
alex50789 <alexander.toribio@utp.ac.pa>
2024-11-06 17:36:31 -0500

    lab8.6

---
+++ LABORATORIO8/lab8.6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class ClaseBase
+{
+    public void test()
+    {
+
+    }
+
+    public virtual void MasTest()
+    {
+
+    }
+}
+
+class ClaseHijo : ClaseBase
+{
+    public override void MasTest() 
+    {
+
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Corrio la aplicacion");
+
+        ClaseHijo hijo = new ClaseHijo();
+        hijo.MasTest();
+    }
+}
