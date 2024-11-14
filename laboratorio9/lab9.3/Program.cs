03f7e52295acd5bd633ce0a4f71db36aa92f2ed9
alex50789 <alexander.toribio@utp.ac.pa>
2024-11-13 19:09:20 -0500

    lab9.3

---
+++ laboratorio9/lab9.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer lado del triángulo: ");
+        double lado1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo lado del triángulo: ");
+        double lado2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer lado del triángulo: ");
+        double lado3 = Convert.ToDouble(Console.ReadLine());
+
+        if ((lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1))
+        {
+            Console.WriteLine("Los lados ingresados forman un triángulo.");
+
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("Es un triángulo equilátero (todos los lados son iguales).");
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine("Es un triángulo isósceles (dos lados son iguales).");
+            }
+            else
+            {
+                Console.WriteLine("Es un triángulo escaleno (todos los lados son diferentes).");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Los lados ingresados no forman un triángulo válido.");
+        }
+    }
+}
