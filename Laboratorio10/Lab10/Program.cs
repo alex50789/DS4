64295e09edcbbc8997dc8cdb4a21bda14e263656
alex50789 <alexander.toribio@utp.ac.pa>
2024-11-19 16:35:29 -0500

    lab 10

---
+++ Laboratorio10/Lab10/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hola")
+        Console.WriteLine("Este es mi primer programa de consola")
+    End Sub
+
+End Module
