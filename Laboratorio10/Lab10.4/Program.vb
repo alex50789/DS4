4ef5649feb50f59ce77db30a9804241f53cf9577
alex50789 <alexander.toribio@utp.ac.pa>
2024-11-19 18:54:12 -0500

    lab10.2

---
+++ Laboratorio10/Lab10.4/Program.vb
@@ -0,0 +1,20 @@
+﻿Public Module Program
+    Public Sub Main(args() As String)
+        Dim perrito As Perro = New Perro()
+        perrito.nombre = "Chizu"
+        perrito.raza = "Husky"
+        perrito.altura = "100cm"
+
+        Console.WriteLine(perrito.comer("Carne"))
+
+        Dim perrito2 As Perro = New Perro()
+        perrito2.nombre = "Helios"
+        perrito2.altura = "0.80cm"
+
+        Console.WriteLine(perrito2.comer("Pollo"))
+
+        Dim perrito3 As Perro = New Perro("Peluchin", "Mastin", "120")
+
+        Console.WriteLine(perrito3.comer("Pan"))
+    End Sub
+End Module
