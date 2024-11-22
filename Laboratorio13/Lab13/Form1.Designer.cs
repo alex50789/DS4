da8ff624d171844eb375697b41800edce09d4873
alex50789 <alexander.toribio@utp.ac.pa>
2024-11-22 02:43:58 -0500

    lab13

--- Laboratorio13/Lab13/Form1.Designer.cs
+++ Laboratorio13/Lab13/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(251, 57);
+            button1.Name = "button1";
+            button1.Size = new Size(227, 64);
+            button1.TabIndex = 0;
+            button1.Text = "Conectar y desconectar de SQL Server";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(231, 136);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(270, 229);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private ListBox listBox1;
     }
 }
--- Laboratorio13/Lab13/Form1.cs
+++ Laboratorio13/Lab13/Form1.cs
@@ -1,10 +1,27 @@
+using System.Data;
+using System.Data.SqlClient;
 namespace Lab13
 {
     public partial class Form1 : Form
     {
+        string connectionString = @"Server= DESKTOP-KVRUI1Q\SQLEXPRESS;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(connectionString);
+            conexion.Open();
+            MessageBox.Show("Se abrio la conexion con el servidor SQL Server y se selecciono la base de  datos");
+            conexion.Close();
+            MessageBox.Show("Se cerro la conexion");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
